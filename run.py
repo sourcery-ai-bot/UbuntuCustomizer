275f48765d32ccb107999ae50403896b29a2942f
11 <6pirule@gmail.com>
2020-03-27 13:46:30 +0200

    add some packages

--- run.py
+++ run.py
@@ -3,10 +3,21 @@ import subprocess
 
 
 class UbuntuCustomizer(object):
-    def __init__(self):
+
+    def install(self):
         #self.update_system()
         #self.update_system()
-        self.install_git()
+        #self.install_from_ubuntu_software()
+        self.install_from_pip3()
+        # self.install_chrome()
+        # self.install_vscode()
+        
+    def install_from_pip3(self):
+        self.pip3_install("virtualenv")
+        self.pip3_install("virtualenvwrapper")
+
+    def execute_command(self, command):
+        subprocess.run(command, shell=True)
 
     def update_system(self):
         subprocess.run(["apt-get", "update"])
@@ -14,13 +25,34 @@ class UbuntuCustomizer(object):
     def upgrade_system(self):
         subprocess.run(["apt-get", "upgrade"])
 
-    def install_git(self):
-        #subprocess.run(["apt-get", "install", "git"])
-        #subprocess.run(["apt-get", "install", "keepassx"])
-        subprocess.run("wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - ", shell=True)
-        subprocess.run("sudo sh -c 'echo \"deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main\" >> /etc/apt/sources.list.d/google.list'", shell=True)
+    def apt_install(self, package_name):
+        subprocess.run(["apt-get", "install", package_name, "-y"])
+
+    def pip3_install(self, package_name):
+        subprocess.run(["pip3", "install", package_name])
+
+    def install_chrome(self):
+        execute_command("wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - ")
+        execute_command("sudo sh -c 'echo \"deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main\" >> /etc/apt/sources.list.d/google.list'")
         self.update_system()
-        subprocess.run(["apt-get", "install", "google-chrome-stable"])
+        apt_install("google-chrome-stable")
 
+    def install_vscode(self):
+        execute_command("curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg")
+        execute_command("sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft-archive-keyring.gpg")
+        execute_command("sudo sh -c 'echo \"deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main\" > /etc/apt/sources.list.d/vscode.list'")
+        apt_install("apt-transport-https")
+        self.update_system()
+        apt_install("code")
 
-UbuntuCustomizer()
+    def install_from_ubuntu_software(self):
+        # self.apt_install("curl")
+        # self.apt_install("git")
+        # self.apt_install("keepassx")
+        # self.apt_install("telegram-desktop")
+        self.apt_install("python3-pip")
+
+if __name__ == "__main__":
+	UbuntuCustomizer().install()
+
+#subprocess.run("", shell=True)
